fd5565e7ac94f476c706a5a2dee51b6a6b53e726
etozhunya <49534738+etozhunya@users.noreply.github.com>
2021-06-05 17:28:13 +0300

    das

--- Compasswell/app/js/app.js
+++ Compasswell/app/js/app.js
@@ -1,118 +1,125 @@
-// // Import jQuery module (npm i jquery)
-// import $ from 'jquery'
-// window.jQuery = $
-// window.$ = $
-
-// // Import vendor jQuery plugin example (not module)
-// require('~/app/libs/mmenu/dist/mmenu.js')
-
 document.addEventListener('DOMContentLoaded', () => {
+  
+	if ($('body').hasClass('haveSlider')) {
+				  if ($(window).width() < 1025) {
+			        $('.slider').slick({
+			          centerMode: true,
+			          centerPadding: '60px',
+			          slidesToShow: 1,
+			          arrows: false,
+			          responsive: [
+			            {
+			              breakpoint: 768,
+			              settings: {
+			                arrows: false,
+			                centerMode: true,
+			                centerPadding: '0px',
+			                slidesToShow: 1
+			              }
+			            }
+			          ]
+			        });
+			    }
+			    $('.slider3').slick({
+			      infinite: true,
+			      slidesToShow: 3,
+			      slidesToScroll: 3,
+			      dots: true,
+			      centerPadding: '0px',
+			      arrows: true,
+			      responsive: [
+			        {
+			          breakpoint: 769,
+			          settings: {
+			            centerMode: true,
+			            centerPadding: '0px',
+			            slidesToShow: 1
+			          }
+			        },
+			        {
+			        	breakpoint: 767,
+			        	settings: {
+			        	  arrows: false,
+			        	  slidesToShow: 1,
+			        	  slidesToScroll: 1
+			        	}
+			        }
+			      ]
+			    });
+			    $('.slider4').slick({
+			      infinite: true,
+			      slidesToShow: 1,
+			      slidesToScroll: 1,
+			      dots: true,
+			      centerPadding: '0px',
+			      // responsive: [
+			      //   {
+			      //     breakpoint: 769,
+			      //     settings: {
+			      //       centerMode: true,
+			      //       centerPadding: '0px',
+			      //       slidesToShow: 1
+			      //     }
+			      //   },
+			      //   {
+			      //   	breakpoint: 767,
+			      //   	settings: {
+			      //   	  arrows: false,
+			      //   	  slidesToShow: 1,
+			      //   	  slidesToScroll: 1
+			      //   	}
+			      //   }
+			      // ]
+			    });
+	}
+	$('.input').on('input', function () {
+ 	    if ( $('#confirmation').val().length > 0 && $('#address').val().length > 0 ) {
+ 	    	$('.deliverButton').removeClass('disabled')
+ 	    } else
+ 	    	$('.deliverButton').addClass('disabled')	
+	});
+
+	$('#adults').on('input', function () {
+ 	    if ( $(this).val().length > 0  ) {
+ 	    	console.log('11a')
+ 	    } 
+	});
+
+	$('.guests').click(function() {
+		$('.function').toggleClass('active')
+	})
 
 
-	// $('.specTable li').click(function() {
-	//     $(this).toggleClass('active')
-	//     $(this).next('.animated').slideToggle(500)
-	//     $(this).siblings().removeClass('active')
-	//     $(this).siblings().find('.animated').slideUp(500)
-	//     $(this).siblings().find('h4').removeClass('active')
-
-	//     var that = $(this)
-	//     window.setTimeout(function() {
-	//         $('html, body').animate({
-	//             scrollTop: that.offset().top - 100
-	//         });
-	//     }, 600)
-	// });
 
 
-	// $('.slider').slick({
-	//     slidesToShow: 1,
-	//     slidesToScroll: 1,
-	//     centerMode: true,
-	//     centerPadding: '10px',
-	//     infinite: true,
-	//     responsive: [{
-	//         breakpoint: 767,
-	//         settings: {
-	//             slidesToShow: 1,
-	//             slidesToScroll: 1
-	//         }
-	//     }]
-	// });
-	  
-		if ($('body').hasClass('haveSlider')) {
-					  if ($(window).width() < 1025) {
-				        $('.slider').slick({
-				          centerMode: true,
-				          centerPadding: '60px',
-				          slidesToShow: 1,
-				          arrows: false,
-				          responsive: [
-				            {
-				              breakpoint: 768,
-				              settings: {
-				                arrows: false,
-				                centerMode: true,
-				                centerPadding: '0px',
-				                slidesToShow: 1
-				              }
-				            }
-				          ]
-				        });
-				    }
-				    $('.slider3').slick({
-				      infinite: true,
-				      slidesToShow: 3,
-				      slidesToScroll: 3,
-				      dots: true,
-				      centerPadding: '0px',
-				      arrows: true,
-				      responsive: [
-				        {
-				          breakpoint: 769,
-				          settings: {
-				            centerMode: true,
-				            centerPadding: '0px',
-				            slidesToShow: 1
-				          }
-				        },
-				        {
-				        	breakpoint: 767,
-				        	settings: {
-				        	  arrows: false,
-				        	  slidesToShow: 1,
-				        	  slidesToScroll: 1
-				        	}
-				        }
-				      ]
-				    });
-				    $('.slider4').slick({
-				      infinite: true,
-				      slidesToShow: 1,
-				      slidesToScroll: 1,
-				      dots: true,
-				      centerPadding: '0px',
-				      // responsive: [
-				      //   {
-				      //     breakpoint: 769,
-				      //     settings: {
-				      //       centerMode: true,
-				      //       centerPadding: '0px',
-				      //       slidesToShow: 1
-				      //     }
-				      //   },
-				      //   {
-				      //   	breakpoint: 767,
-				      //   	settings: {
-				      //   	  arrows: false,
-				      //   	  slidesToShow: 1,
-				      //   	  slidesToScroll: 1
-				      //   	}
-				      //   }
-				      // ]
-				    });
-		}
 
+
+
+	if ( $('#adults').val() > 0 ) {
+				console.log('sdasdasda')
+			}
+	$('#adults').on('input', function () {
+			if ( $(this).val() > 0 ) {
+				console.log('sdasdasda')
+			}
+	})	
+
+	if ( $('#adults').val() == 0 ) {
+	    $('#adults').addClass('disabled')
+	} else
+		$('#adults').removeClass('disabled')
+	
+
+	$('.calc').click(function() {
+	        var val = $(this).siblings('input').val();
+
+	        if ($(this).val() == 'plus') {
+	            val++;
+	        } else if(val > 0) {
+	            val--;
+	        }
+	        $(this).siblings('input').val(val);
+	    })
 
 
 	$('.specTable li').click(function() {
@@ -124,13 +131,6 @@ document.addEventListener('DOMContentLoaded', () => {
 	        $(this).find('.animated').slideDown();
 	    } else
 	        $(this).find('.animated').slideUp();
-	    // var that = $(this)
-     //    window.setTimeout(function() {
-	    //     $('html, body').animate({
-	    //         scrollTop: that.offset().top - 100
-	    //     });
-	    // }, 600)
-
 	});
 	
 	$('.steps li').click(function() {
