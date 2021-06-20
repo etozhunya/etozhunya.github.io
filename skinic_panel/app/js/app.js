ebc39dd87ea6922f6dee822354122d99171de80e
etozhunya <49534738+etozhunya@users.noreply.github.com>
2021-06-21 01:07:55 +0300

    das

--- skinic_panel/app/js/app.js
+++ skinic_panel/app/js/app.js
@@ -8,9 +8,14 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
+	$('.select').select2()
 	$('.side-bar__list li a').click(function(event) {
 	    event.preventDefault();
 	});
+
+	$('.side-bar__hamburger').click(function() {
+		$('.page').toggleClass('wide')
+	})
 
 	$('.click-item').click(function() {
 		$(this).addClass('active').siblings('').removeClass('active')
@@ -20,6 +25,85 @@ document.addEventListener('DOMContentLoaded', () => {
 		} else {
 			$('.edit-logo').removeClass('edit')
 		}
+	})
+
+	// HEADER BUTTONS CLICK
+	$('#NA-btn').click( function() {
+		$('.animated__order .step-item').removeClass('in-view').eq(0).addClass('in-view')
+		$('.modal').addClass('visible')
+		$('.modal-new-appt').addClass('active')
+	})
+
+	$('.back-to-date-button').click(function() {
+		$('.animated__order .step-item').removeClass('in-view').eq(0).addClass('in-view')
+	})
+
+	$('#BH-btn').click( function() {
+		$('.modal').addClass('visible')
+		$('.modal-block-hours').addClass('active')
+	})
+
+	// FILTER CALENDAR BUTTONS
+	$(document).click(function(event) { 
+	  var $target = $(event.target);
+	  if($(event.target).closest('.content-wrapper__filter-calendar').length == 0)
+	  {
+	   $('.filter-calendar-buttons').removeClass('active')
+	  }        
+	});
+
+	$('#open-filter').click(function() {
+		$('.filter-container').addClass('active')
+	})
+
+
+
+
+
+	// ACARDEOUN
+
+	$('.headings li h4').click(function() {
+	    $(this).parent().toggleClass('active')
+	    $(this).toggleClass('active');
+	    $(this).next('.animated').slideToggle(300)
+	    $(this).parent().siblings().removeClass('active')
+	    $(this).parent().siblings().find('.animated').slideUp(300)
+	    $(this).parent().siblings().find('h4').removeClass('active')
+
+	    var that = $(this)
+	    window.setTimeout(function() {
+	        $('html, body').animate({
+	            scrollTop: that.offset().top - 100
+	        });
+	    }, 400)
+	});
+	$('#sel4').on('select2:select', function (e) { 
+	     	$(this)
+	     	.parent().slideUp(300)
+	     	.prev().removeClass('active')
+	     	.parents('li').next().find('h4').addClass('active')
+	     	.next().slideToggle(300)
+	});
+	$('.animated__steps .click-item').click(function() {
+		console.log('aasdad')
+		$(this)
+			.parents('.animated').slideUp(300)
+			.parents('li').find('h4').removeClass('active')
+			.parents('li').next().find('h4').addClass('active')
+			.next().slideDown(300)
+
+	})
+
+	// $(document).on("click",".", function(){
+	//     $('.timePicker').removeClass('disabled')
+	//    $('.timeSelect').select2('open');
+	// });
+
+
+
+
+	$('.calendar-min-section .cal-next-step').click(function () {
+		$('.animated__steps.in-view').next().addClass('in-view').siblings().removeClass('in-view')
 	})
 
 
@@ -42,26 +126,18 @@ document.addEventListener('DOMContentLoaded', () => {
 		$('.reschedule-block--step2 .next-step').addClass('disabled')
 		$('.form .click-item').removeClass('active')
 		$('#step-textarea').val('')
-
 	})
-
-
-
 
 
 	
 
-	$('.select').select2()
-
-	$('.side-bar__hamburger').click(function() {
-		$('.page').toggleClass('wide')
-	})
-
+	// FLEX TABLE SLOT CLICK
 	$('.table-slot').click(function() {
 		if($(this).hasClass('block-hours')) {
 			$(this).off('click')
 		}
 		$('.modal').addClass('visible')
+		$('.modal-details__head .click-item').removeClass('active')
 		$('.modal-details__create-note').removeClass('visible')
 		$('.modal-details').addClass('active')
 		$('.info__wrapper__block').removeClass('in-view').eq(0).addClass('in-view')
@@ -69,38 +145,71 @@ document.addEventListener('DOMContentLoaded', () => {
 	})
 
 
-	// buttons
-	$('.close-button').click(function(){
-		$('.edit-logo').removeClass('edit')
+	// buttons next prev
+
+	$('.close-button').click(function() {
 		$('.modal').removeClass('visible')
 		$('.modal-block').removeClass('active')	
-		$('.info__wrapper__block').removeClass('in-view').eq(0).addClass('in-view')
-		$('.modal-details__head .click-item').removeClass('active')
 	})
-	
+	$(document).on("keyup", function (e) { 
+	  if (e.keyCode === 27) { 
+	    $('.modal').removeClass('visible')
+			$('.modal-block').removeClass('active')	
+	  }
+	});
+
 	$('.back-button').click(function() {
 		$('.edit-logo').removeClass('edit')
 		$('.modal-details__create-note').removeClass('visible')
+
+	
+		// CLOSE	MODAL
+  
 		if($('.home__block').hasClass('in-view')) {
-			$(this).addClass('close-button')
-		} else {
-			$('.back-button').removeClass('close-button')
-		}
-		if ($(this).hasClass('close-button')) {
+	
 			$('.modal').removeClass('visible')
 			$('.modal-block').removeClass('active')	
+			$('.info__wrapper__block').removeClass('in-view').eq(0).addClass('in-view')
+			$('.modal-details__head .click-item').removeClass('active')
 		}
-		$('.modal-details__head .click-item').removeClass('active')
-		$('.info__wrapper__block').removeClass('in-view').eq(0).addClass('in-view')
+		// CLOSE	MODAL END
 
+
+		// RESCHEDULE	MODAL
+		if ($('.reschedule-block--step4').hasClass('in-view')) {
+			console.log('LAST')
+			$('.info__wrapper__block').removeClass('in-view').eq(0).addClass('in-view')
+		}
+		if ($('.reschedule-block--step1').hasClass('in-view') && $('.reschedule-block').hasClass('in-view') ) {
+					console.log('11111')
+					$('.modal-details__head .click-item').removeClass('active')
+					
+					$('.info__wrapper__block').removeClass('in-view').eq(0).addClass('in-view')
+					return
+			}
+
+		if ($('.reschedule-block').hasClass('in-view')) {
+			$('.step-item.in-view').prev().addClass('in-view').siblings().removeClass('in-view')
+			console.log('home')
+		}
+		if (!$('.reschedule-block').hasClass('in-view')) {
+			$('.info__wrapper__block').removeClass('in-view').eq(0).addClass('in-view')	
+			$('.modal-details__head .click-item').removeClass('active')
+		}
+		
 	})
 
+	// $('.inside-button').click(function(){
+	// 	$('.step-item').removeClass('in-view').prev().addClass('in-view')
+	// })
 
 
-	////
+
+	////datepicker
 	datepicker("#open-calendar",{})
 
 
+	// modals
 	$('.add-note').click(function() {
 		$('.modal-details__create-note').addClass('visible')
 	})
@@ -155,6 +264,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 })
 
+
 // SIMULATIN CLICKS FULL CALLENDAR
 $('.TWM__days span').click(function(){
 	if(!$('.global-list-button').hasClass('active')) {
@@ -190,14 +300,6 @@ var yyyy = today.getFullYear();
 
 today = 'Today ' + monthNames[today.getMonth()] + ' ' + dd + ' st, ' + yyyy;
 text.innerHTML = today
-
-// var y = document.getElementsByClassName('fc-toolbar-title');
-// var aNode = y[0];
-// console.log(y)
-
-
-
-// CALENDAR VANILLA JS
 
 document.addEventListener('DOMContentLoaded', function() {
         var calendarEl = document.getElementById('calendar');
